d170ae99416aa66bf358954ac29f6b33a5493ec3
OengGechty <gechtyoeng@gmail.com>
2025-02-13 11:43:32 +0700

    first commit

---
+++ S2-PRACTICE-ES6/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let found = SHOPPING_CART.find((f)=> f.id === productId);
+  // if(found){
+  //   found.quantity--;
+  // }else{
+  //   SHOPPING_CART.splice(SHOPPING_CART.indexOf(found),1);
+  // }
